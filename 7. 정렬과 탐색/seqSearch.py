11b7a4326125820c84d549b7df36de6ff1429a9d
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-06-13 22:50:00 +0900

    seqSearch(순차 탐색)
    가장 간단함.

---
+++ 7. 정렬과 탐색/seqSearch.py
@@ -0,0 +1,45 @@
+# 순차 탐색
+# 정렬되지 않은 배열에 적용 가능
+# 배열을 처음부터 마지막까지 하나씩 검사
+# 평균 비교 횟수: (n+1)/2번 비교 (최악의 경우 n번)
+# 찾고 싶은 대상을 key라고 함.
+import random
+
+def insertionSort(A):
+    n = len(A)
+
+    for i in range(1, n):
+        key = A[i]
+        j = i - 1
+
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j -= 1
+
+        A[j + 1] = key
+
+def seqSearch(A, key):
+    n = len(A)
+
+    for i in range(n):
+        if A[i] == key:
+            return i            # 인덱스 return
+
+    return -1
+
+if __name__ == "__main__":
+    A = []
+    for i in range(15):
+        A.append(random.randint(1, 100))
+
+    insertionSort(A)
+    print("A =", A)
+
+    key = int(input("Input Search Key : "))
+
+    idx = seqSearch(A, key)
+
+    if idx != -1:
+        print("%d 위치에서 발견" % idx)
+    else:
+        print("키가 존재하지 않습니다.")
