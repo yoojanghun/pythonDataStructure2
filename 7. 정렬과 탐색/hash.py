2dfcba004b5a97b4a6837cb711c2be8806c1141b
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-06-14 12:55:43 +0900

    hash(선형 조사법)

---
+++ 7. 정렬과 탐색/hash.py
@@ -0,0 +1,68 @@
+# 선형 조사법
+# 충돌이 일어나면 해시 테이블의 다음 위치에 저장
+
+# 빈 버킷을 두 가지로 분류
+# 한번도 사용하지 않은 것 => 0
+# 사용 되었다가 삭제되어 현재 비어있는 것 => -1
+M = 13
+
+class HashTable:
+    def __init__(self):
+        self.table = [0] * M
+
+    def hashFn(self, key):
+        return key % M
+
+    def insert(self, key):
+        hashVal = self.hashFn(key)
+
+        for i in range(M):
+            bucket = (hashVal + i) % M
+
+            if self.table[bucket] == 0:
+                self.table[bucket] = key
+                break
+
+    def search(self, key):
+        hashVal = self.hashFn(key)
+
+        for i in range(M):
+            bucket = (hashVal + i) % M
+
+            if self.table[bucket] == 0:
+                return -1
+            elif self.table[bucket] == key:
+                return bucket
+
+    def delete(self, key):
+        hashVal = self.hashFn(key)
+
+        for i in range(M):
+            bucket = (hashVal + i) % M
+
+            if self.table[bucket] == 0:
+                print("No key to delete")
+                return
+            elif self.table[bucket] == key:
+                print("Delete Key(%d) at bucket(%d). " % (key, bucket))
+                HT.table[bucket] = -1
+                return
+
+
+if __name__ == "__main__":
+
+    HT = HashTable()
+    data = [45, 27, 88, 9, 71, 60, 46, 38, 24]
+
+    for d in data:
+        print("h(%d) = %2d " % (d, HT.hashFn(d)))
+        HT.insert(d)
+        print(HT.table)
+
+    print()
+    print("Search(60) ---> ", HT.search(60))
+
+    HT.delete(9)
+    print(HT.table)
+
+    print("Search(46) --> ", HT.search(46))
